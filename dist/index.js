24fe0746873462ce7c2426ec812f7e7d766d27cb
saulomorais <litodev97@gmail.com>
2025-04-05 14:04:48 -0300

    addbabel

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("./app.js"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+_app["default"].run();
